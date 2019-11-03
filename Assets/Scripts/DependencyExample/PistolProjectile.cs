25533e3abfd198f21e73b5f2d4b3052ff0fc8056
Postea <ydb1997@hotmail.de>
2019-11-03 21:08:49 +0100

    Sucessfully made a unidirectional polymorph dependency between components, projectile, cannonprojectile, pistolprojectile and towershooting as example scripts

---
+++ Assets/Scripts/DependencyExample/PistolProjectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolProjectile : Projectile
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public override void myNameIs()
+    {
+        Debug.Log("I am a Pistol Projectile!");
+    }
+}
---
+++ Assets/Scripts/DependencyExample/TowerShooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TowerShooting : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Projectile x = GetComponent<Projectile>();
+        x.myNameIs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
