e2468e07efd9a2a1b5baf6b11ae6acbc6553a985
Postea <ydb1997@hotmail.de>
2019-11-03 16:05:40 +0100

    New allrounder script for Towers, with dynamic target-list based on collision enters/exits

--- Assets/Scripts/TriggerOnCollide.cs
+++ Assets/Scripts/TriggerOnCollide.cs
@@ -4,18 +4,4 @@ using UnityEngine;
 
 public class TriggerOnCollide : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Entered");
-    }
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Stays");
-
-    }
-    void OnTriggerExit(Collider other)
-    {
-        Debug.Log("Exited");
-
-    }
 }
