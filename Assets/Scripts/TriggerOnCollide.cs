3bcdc4829ca0862873211e36956b06e5c0850c63
Postea <ydb1997@hotmail.de>
2019-11-03 14:55:21 +0100

    Changed scene slightly

---
+++ Assets/Scripts/TriggerOnCollide.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOnCollide : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered");
+    }
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Stays");
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exited");
+
+    }
+}
