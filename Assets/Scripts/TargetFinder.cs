917f44de0db4e7dda9714048abfcdd44a3c66702
Postea <ydb1997@hotmail.de>
2019-11-04 13:53:48 +0100

    First good tower shooting example

---
+++ Assets/Scripts/TargetFinder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFinder : MonoBehaviour
+{
+    private List<GameObject> _targets;
+    public GameObject target
+    {
+        get
+        {
+            if (_targets.Count != 0)
+                return _targets[0];
+            else
+                return null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targets = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Collision
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _targets.Add(other.gameObject);
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Target out of range, its " + other.gameObject);
+        _targets.Remove(other.gameObject);
+
+    }
+
+}
---
+++ Assets/Scripts/TowerShooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShooting : MonoBehaviour
+{
+    private Projectile projectile;
+    private TargetFinder targetfinder;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile = GetComponent<Projectile>();
+        targetfinder = GetComponent<TargetFinder>();
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetfinder.target!=null)
+        {
+            shoot();
+        }
+        else
+        {
+            Debug.Log("No target");
+
+        }
+    }
+
+    void shoot()
+    {
+        Debug.Log("Shooting at: " +targetfinder.target);
+    }
+}
