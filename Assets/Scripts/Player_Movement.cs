14c959cd59de09efd5a09756750119e6ca5d1358
s72588 <s72588@beuth-hochschule.de>
2019-11-18 19:25:52 +0100

    rebased from master, mousetargeting working, charactermousemovement weird

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -5,24 +5,108 @@ using UnityEngine.Tilemaps;
 
 public class Player_Movement : MonoBehaviour
 {
+    [SerializeField] Tilemap tilemap;
+    [SerializeField] GameObject player;
     public float moveSpeedMax = 3f;
     public Rigidbody2D rb;
-    public float rotation;
-    public Tilemap tilemap;
+    public int facing;
+    public bool isMovingByKey;
+    public bool isMoving;
+    TileBase clickedTile;
+    Vector3 clickPos;
     Vector2 input;
-    Vector2 velocity = new Vector2(0, 0);
-    public Vector2 Velocity { get => velocity; set => velocity = value; }
+    Vector2 velocity;
     Vector2 moveTo;
-    
-    /*
-    bool movingLeft;
-    bool movingRight;
-    bool movingUp;
-    bool movingDown;
-    */
+    Tile_Targeting targetingscript;
+
+    void Start()
+    {
+        //not facing anything, so no tile targeted
+        facing = 0;
+        player = this.gameObject;
+        isMoving = false;
+    }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMovingByKey = false;
+            facing = 0;
+            clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(string.Format("clickPos [X: {0} Y: {1}]", clickPos.x, clickPos.y));
+            if (clickPos.x < 0) { clickPos.x--; }
+            if (clickPos.y < 0) { clickPos.y--; }
+            clickedTile = tilemap.GetTile(new Vector3Int((int)clickPos.x, (int)clickPos.y, 0));
+        }
+        if (Input.anyKeyDown && !Input.GetMouseButtonDown(0))
+        {
+            isMovingByKey = true;
+            clickedTile = null;
+        }
+        if (isMovingByKey) { KeyBoardMovement(); }
+        if (!isMovingByKey) { MouseMovement(); }
+    }
+    void FixedUpdate()
+    {
+        if (velocity.x != 0 || velocity.y != 0)
+        {
+            isMoving = true;
+            //Move to Direction
+            moveTo = rb.position + velocity * Time.fixedDeltaTime;
+            rb.MovePosition(moveTo);
+            //Determine facing
+            //X-Axis
+            if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
+            {
+                if (velocity.x > 0)
+                {
+                    facing = 2;
+                }
+                else { facing = 4; }
+            }
+            //Y-Axis
+            else
+            {
+                if (Mathf.Abs(velocity.x) < Mathf.Abs(velocity.y))
+                {
+                    if (velocity.y > 0)
+                    {
+                        facing = 1;
+                    }
+                    else { facing = 3; }
+                }
+            }
+        }
+        else { isMoving = false; }
+    }
+    void MouseMovement()
+    {   
+        
+        Vector2 toClick = new Vector2(clickPos.x - rb.position.x, clickPos.y - rb.position.y); //vector from character to click
+        float mag = toClick.magnitude; //length of toClick
+        if (mag >= 0.2)
+        {
+            float div = mag / moveSpeedMax; //how many times allowed movespeed fits in length
+            if (div > 1.0) //if a certain distance away
+            {
+                velocity = new Vector2(toClick.x / div, toClick.y / div); //set to click as velocity, shortened to respect allowed movespeed
+            }
+            else
+            {
+                velocity = toClick * div; //if close by, do weird calculations to get a tolerable movespeed while slowing down
+                if (velocity.x <= 0.5 && velocity.y <= 0.5)
+                {
+                    velocity = new Vector2(0, 0);
+                    player.GetComponent<Tile_Targeting>().MouseTargetTile(clickPos);
+                }
+            }
+        }
+        else { velocity = new Vector2(0, 0); } //when very close by, stop
+
+    }
+    void KeyBoardMovement()
     {
         //get input
         input.x = Input.GetAxisRaw("Horizontal");
@@ -30,17 +114,19 @@ public class Player_Movement : MonoBehaviour
         //Stop or slow down if no input
         if (input.x == 0)
         {
-            if(Mathf.Abs(velocity.x) <= 0.05)
+            if (Mathf.Abs(velocity.x) <= 0.05)
             {
                 velocity.x = 0;
-            } else { velocity.x *= (float)0.75; }                      
+            }
+            else { velocity.x *= (float)0.75; }
         }
         if (input.y == 0)
         {
             if (Mathf.Abs(velocity.y) <= 0.05)
             {
-                velocity.y = 0;             
-            } else { velocity.y *= (float)0.75; }                      
+                velocity.y = 0;
+            }
+            else { velocity.y *= (float)0.75; }
         }
         //add input to velocity
         velocity += input;
@@ -62,37 +148,5 @@ public class Player_Movement : MonoBehaviour
             velocity.y = -moveSpeedMax;
         }
     }
-    void FixedUpdate()
-    {
-        
-        if(velocity.x != 0 || velocity.y != 0)
-        {
-            rotation = Vector2.Angle(new Vector2(0, 1), velocity);
-            if (velocity.x > 0)
-            {
-                rotation = -rotation;
-            }
-            rb.MoveRotation(rotation);
-            //Move to Direction
-            moveTo = rb.position + velocity * Time.fixedDeltaTime;
-            rb.MovePosition(moveTo);
-        }
-        /*
-         //Determine direction to rotate to
-         if (Input.GetKeyDown("down")) { movingDown  = true; }
-         if (Input.GetKey("up"))       { movingUp    = true; }
-         if (Input.GetKey("left"))     { movingLeft  = true; }
-         if (Input.GetKey("right"))    { movingRight = true; }
-         //Rotate to Direction
 
-         if ( movingUp && !movingDown && !movingLeft && !movingRight) { rb.MoveRotation(  0); }
-         if ( movingUp && !movingDown && !movingLeft &&  movingRight) { rb.MoveRotation( 45); }
-         if (!movingUp && !movingDown && !movingLeft &&  movingRight) { rb.MoveRotation( 90); }
-         if (!movingUp &&  movingDown && !movingLeft &&  movingRight) { rb.MoveRotation(135); }
-         if (!movingUp &&  movingDown && !movingLeft && !movingRight) { rb.MoveRotation(180); }
-         if (!movingUp &&  movingDown &&  movingLeft && !movingRight) { rb.MoveRotation(225); }
-         if (!movingUp && !movingDown &&  movingLeft && !movingRight) { rb.MoveRotation(270); }
-         if ( movingUp && !movingDown &&  movingLeft && !movingRight) { rb.MoveRotation(315); }
-         */
-    }
 }
--- Assets/Scripts/Tile_Targeting.cs
+++ Assets/Scripts/Tile_Targeting.cs
@@ -6,22 +6,23 @@ using UnityEngine.Tilemaps;
 public class Tile_Targeting : MonoBehaviour
 {
     public Tile tileHighlighter; //Tile used to mark targeted Tile
-    public Tilemap tilemap;
+    [SerializeField] Tilemap tilemap;
     public Tile targetedTile; //saves tile "under" tileHighlighter, is the targeted tile
-    float playerRot;
+    int playerFacing;
     Vector3Int playerPos;
     Vector3Int targetLoc; //Coordinates to be targeted
-    Vector3Int targetLocOld; //Coordinates of last targeted Tile
+    Vector3Int targetLocOld; //Coordinates of last targeted Tile, to restore it if highlighter moves
     Player_Movement movementscript;
     [SerializeField] GameObject player;
 
     void Start()
     {
         movementscript = player.GetComponent<Player_Movement>();
-        playerRot = movementscript.rotation;
+        playerFacing = movementscript.facing;
         targetLocOld = targetLoc;
+        player = this.gameObject;
+
     }
-    // Update is called once per frame
     /*
     void Update()
     {
@@ -30,36 +31,51 @@ public class Tile_Targeting : MonoBehaviour
     */
     private void FixedUpdate()
     {
+        if (movementscript.isMovingByKey)
+        {
+            Vector3Int playerPos = tilemap.WorldToCell(transform.position);
+            playerFacing = movementscript.facing;
+            if (Mathf.Round(playerFacing) == 1)
+            {
+                targetLoc = new Vector3Int(playerPos.x, playerPos.y + 1, 0);
+            }
+            if (Mathf.Round(playerFacing) == 2)
+            {
+                targetLoc = new Vector3Int(playerPos.x + 1, playerPos.y, 0);
+            }
+            if (Mathf.Round(playerFacing) == 3)
+            {
+                targetLoc = new Vector3Int(playerPos.x, playerPos.y - 1, 0);
+            }
+            if (Mathf.Round(playerFacing) == 4)
+            {
+                targetLoc = new Vector3Int(playerPos.x - 1, playerPos.y, 0);
+            }
+            if (targetLoc != targetLocOld)
+            {
+                if (targetedTile != null)
+                {
+                    tilemap.SetTile(targetLocOld, targetedTile); //restore old tile
+                }
+                targetedTile = (Tile)tilemap.GetTile(targetLoc);
+                tilemap.SetTile(targetLoc, tileHighlighter);  // set the new tile              
+                targetLocOld = targetLoc; // save the new position for restoring next frame
+            }
+        }
+    }
+    public void MouseTargetTile(Vector3 clickPos)
+    {
         
-        Vector3Int playerPos = tilemap.WorldToCell(transform.position);
-        playerRot = movementscript.rotation;
-        if (Mathf.Round(playerRot) == 0)
-        {
-            targetLoc = new Vector3Int(playerPos.x, playerPos.y + 1, 0);
-        }
-        if (Mathf.Round(playerRot) == -90)
-        {
-            targetLoc = new Vector3Int(playerPos.x + 1, playerPos.y, 0);
-        }
-        if (Mathf.Round(playerRot) == 180)
-        {
-            targetLoc = new Vector3Int(playerPos.x, playerPos.y - 1, 0);
-        }
-        if (Mathf.Round(playerRot) == 90)
-        {
-            targetLoc = new Vector3Int(playerPos.x - 1, playerPos.y, 0);
-        }
+        targetLoc = new Vector3Int((int)clickPos.x, (int)clickPos.y, 0);
         if (targetLoc != targetLocOld)
         {
-            if(targetedTile != null)
-                {
-                    tilemap.SetTile(targetLocOld, targetedTile);
-                }        
+            if (targetedTile != null)
+            {
+                tilemap.SetTile(targetLocOld, targetedTile); //restore old tile
+            }
             targetedTile = (Tile)tilemap.GetTile(targetLoc);
-            // set the new tile
-            tilemap.SetTile(targetLoc, tileHighlighter);
-            // save the new position for next frame
-            targetLocOld = targetLoc;
-        }                
+            tilemap.SetTile(targetLoc, tileHighlighter); // set the new tile           
+            targetLocOld = targetLoc; // save the new position for restoring next frame
+        }
     }
 }
