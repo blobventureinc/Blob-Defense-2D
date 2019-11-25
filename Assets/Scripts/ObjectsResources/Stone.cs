446f3179fb43862cc1962846f9e9e4517ffb583b
s72588 <s72588@beuth-hochschule.de>
2019-11-25 17:31:52 +0100

    mining process complete, BUG: INFINITE WHILE-LOOP

--- Assets/Scripts/ObjectsResources/Stone.cs
+++ Assets/Scripts/ObjectsResources/Stone.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Stone : MonoBehaviour
 {
     public string type;
-    int value;
+    public int value;
+    public int duration;
     // Start is called before the first frame update
     void Start()
     {
         type = "stone";
         value = 3;
+        duration = 2;
         Debug.Log("STONE INITIALIZED");
     }
 
@@ -21,7 +23,7 @@ public class Stone : MonoBehaviour
     }
 
     public int Mine() {
-        Object.Destroy(gameObject, value);
+        Object.Destroy(gameObject);
         return value;
     }
 
---
+++ Assets/Scripts/Player/Player_Mining.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Mining : MonoBehaviour
+{
+
+    public bool isMining;
+    public int miningTimer;
+    Vector2 input;
+    Stone resourceScript;
+    [SerializeField] private Tile_Targeting targetingScript = null;
+    // Start is called before the first frame update
+    void Start() {
+        isMining = false;
+        miningTimer = 0;
+        input = new Vector2(0, 0);
+        resourceScript = null;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return) && !isMining) {
+            StartCoroutine(Mine());
+        }
+    }
+
+    IEnumerator Mine() {
+        Vector3Int target = targetingScript.gettargetLoc();
+        Debug.Log("SEARCHING AT:" + target.x + " , " + target.y);
+        Collider[] objarr = Physics.OverlapSphere(target, 1);
+        if (objarr.Length == 1) {
+            GameObject ob = objarr[0].gameObject;
+            resourceScript = ob.GetComponent<Stone>();
+            Debug.Log("FOUND: " + resourceScript.type + ", VALUE: " + resourceScript.value);
+            isMining = true;
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            while (ob && input.x == 0 && input.y == 0) {
+                if(miningTimer == resourceScript.duration) {
+                    Object.Destroy(ob);
+                } else {
+                    yield return new WaitForSeconds(1);
+                    input.x = Input.GetAxisRaw("Horizontal");
+                    input.y = Input.GetAxisRaw("Vertical");
+                    miningTimer++;
+                    Debug.Log("INCREMENTING MINING TIMER");
+                }    
+            }
+            isMining = false;
+            miningTimer = 0;
+        }   
+
+    }
+}
