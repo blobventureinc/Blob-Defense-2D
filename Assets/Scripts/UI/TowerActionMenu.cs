7575e16ced19eb3361dde193b89340bfda2c9645
MrInformatic <maxundmoritz99199@gmail.com>
2019-12-01 16:08:22 +0100

    Added Skill UI

--- Assets/Scripts/UI/TowerActionMenu.cs
+++ Assets/Scripts/UI/TowerActionMenu.cs
@@ -14,18 +14,6 @@ public class TowerActionMenu : MonoBehaviour
     // TODO: This Method has to be wired up to an Event
     public void ShowTowerActionMenu(AttributeManager attributeManager, Tower tower) 
     {
-        /*var towerActions = tower.TowerActions;
-        while(towerActions.MoveNext()) 
-        {
-            var towerActionButtonObject = Instantiate(towerActionButtonPrefab, transform);
-            var towerActionButton = towerActionButtonObject.GetComponent<TowerActionButton>();
-
-            towerActionButton.TowerAction = towerActions.Current;
-            towerActionButton.tower = tower;
-            towerActionButton.towerActionMenu = this;
-            
-            buttons.Add(towerActionButtonObject);
-        }*/
         foreach(var towerAction in tower.towerActions) {
             var towerActionButtonObject = Instantiate(towerActionButtonPrefab, transform);
             var towerActionButton = towerActionButtonObject.GetComponent<TowerActionButton>();
