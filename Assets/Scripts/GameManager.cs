c3f74d09356107d23490fc0d90ea078130346045
Florian Taute <info@tauteweb.de>
2019-12-12 16:03:46 +0100

    added States to Gamemanager, added Waves, edited UI and UpdatePlayerAttributes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,14 +9,19 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private AttributeManager player;
     //[SerializeField] private GameObject gameMenu;
+
+    [Header("UI Elements")]
     [SerializeField] private GameObject gameOverUI;
     [SerializeField] private GameObject startRoundButton;
     [SerializeField] private GameObject roundTimerPanel;
+    [SerializeField] private GameObject towerMenu;
     [SerializeField] private Text roundText;
 
+    [Header("Path Spawner")]
     [SerializeField] private Spawner spawner;
+    private int wave = 0;
 
-    [SerializeField] private int wave = 0;
+    private bool isInCombat;
 
     // Start is called before the first frame update
     void Start()
@@ -25,26 +30,37 @@ public class GameManager : MonoBehaviour
         //gameMenu.SetActive(false);
         gameOverUI.SetActive(false);
         startRoundButton.SetActive(true);
+        spawner.waveEnded.AddListener(RoundEnd);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.health.value <= 0) {
-            Time.timeScale = 0.00001f;
-            gameOverUI.SetActive(true);
-        }
-        if (wave == 0) {
-            roundTimerPanel.SetActive(false);
-        } else {
+        if(isInCombat) {
             roundTimerPanel.SetActive(true);
+            startRoundButton.SetActive(false);
+            towerMenu.SetActive(false);
+            if (player.health.value <= 0) {
+                Time.timeScale = 0.00001f;
+                gameOverUI.SetActive(true);
+            }
+        } else {
+            roundTimerPanel.SetActive(false);
+            startRoundButton.SetActive(true);
+            towerMenu.SetActive(true);
         }
     }
 
     public void NextWave() {
+        isInCombat = true;
         spawner.SpawnNextWave();
         wave++;
-        roundText.text = "Wave " + wave + " / 10";
+        roundText.text = "Wave " + wave + " / "+spawner.waves.Length;
+    }
+
+    public void RoundEnd() {
+        isInCombat = false;
+        player.level.Increase(1);
     }
 
     public void OpenMainMenu() {
--- Assets/Scripts/Spawning/Spawner.cs
+++ Assets/Scripts/Spawning/Spawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using PathCreation;
 
 [RequireComponent(typeof(PathCreator))]
@@ -9,6 +10,10 @@ public class Spawner : MonoBehaviour
     public Wave[] waves;
     [HideInInspector]
     public int curentWave;
+
+    private int deathCount = 0;
+    private int enemyWaveCount = 10;
+    public UnityEvent waveEnded;
 
     private PathCreator pathCreator;
 
@@ -36,20 +41,36 @@ public class Spawner : MonoBehaviour
 
     public IEnumerator SpawnEnemyWave(EnemyWave enemyWave) 
     {
+        deathCount = 0;
+        Debug.Log("DeathCount: " + deathCount);
+        enemyWaveCount = enemyWave.ammount;
+        Debug.Log("EnemyWaveAmount: " + enemyWave.ammount);
         yield return new WaitForSeconds(enemyWave.startTime);
         for(int i = 0; i < enemyWave.ammount; i++) 
         {
             GameObject enemy = Instantiate(enemyWave.enemyPrefab, pathCreator.path.GetPointAtDistance(0f), Quaternion.identity, transform);
+            enemy.GetComponent<HealthSystem>().onDeath.AddListener(CountDeath);
             WalkAlongPath walkAlongPath = enemy.GetComponentInChildren<WalkAlongPath>();
             walkAlongPath.pathCreator = pathCreator;
             yield return new WaitForSeconds(enemyWave.interval);
         }
     }
 
+    public void CountDeath() {
+        deathCount++;
+        Debug.Log("DeathCount: " + deathCount);
+    }
+
+    private void Update() {
+        if (deathCount == enemyWaveCount) {
+            waveEnded.Invoke();
+            deathCount = 0;
+        }
+    }
+
     void OnEnable() 
     {
         pathCreator = GetComponent<PathCreator>();
-
         curentWave = -1;
     }
 }
