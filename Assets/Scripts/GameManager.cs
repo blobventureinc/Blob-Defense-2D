de7846b345707b76ba089aa7ecef4d0bca17c1f4
Florian Taute <info@tauteweb.de>
2019-12-12 13:35:25 +0100

    added MainMenu Scene / created MainMenu Script / added GameManager to Scene / implemented GameManager Logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private AttributeManager player;
-    [SerializeField] private GameObject GameOverUI;
+    //[SerializeField] private GameObject gameMenu;
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private GameObject startRoundButton;
+    [SerializeField] private GameObject roundTimerPanel;
+    [SerializeField] private Text roundText;
+
+    [SerializeField] private Spawner spawner;
+
+    [SerializeField] private int wave = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1.0f;
+        //gameMenu.SetActive(false);
+        gameOverUI.SetActive(false);
+        startRoundButton.SetActive(true);
     }
 
     // Update is called once per frame
@@ -17,7 +32,22 @@ public class GameManager : MonoBehaviour
     {
         if(player.health.value <= 0) {
             Time.timeScale = 0.00001f;
-            GameOverUI.SetActive(true);
+            gameOverUI.SetActive(true);
         }
+        if (wave == 0) {
+            roundTimerPanel.SetActive(false);
+        } else {
+            roundTimerPanel.SetActive(true);
+        }
+    }
+
+    public void NextWave() {
+        spawner.SpawnNextWave();
+        wave++;
+        roundText.text = "Wave " + wave + " / 10";
+    }
+
+    public void OpenMainMenu() {
+        SceneManager.LoadScene("MainMenu");
     }
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartNewGame() {
+        SceneManager.LoadScene("Master");
+    }
+
+    public void LoadGame() {
+
+    }
+
+    public void KeyBindMenu() {
+
+    }
+
+    public void Scoreboard() {
+
+    }
+
+    public void QuitGame() {
+
+    }
+}
