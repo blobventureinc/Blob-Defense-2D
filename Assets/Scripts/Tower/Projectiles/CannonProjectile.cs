3b4dcef87ed7bc68cda3bb07f4ce6c794a26ca3c
Postea <ydb1997@hotmail.de>
2019-12-13 21:14:31 +0100

    Fixed the projectiles, so they do not just disapear if the target dies. Now they travel to the last position of the target

--- Assets/Scripts/Tower/Projectiles/CannonProjectile.cs
+++ Assets/Scripts/Tower/Projectiles/CannonProjectile.cs
@@ -6,14 +6,18 @@ public class CannonProjectile : Projectile
 {
     public override void Start()
     {
-        _target.GetComponent<HealthSystem>().onDeath.AddListener(DestroyItself);
+        //_target.GetComponent<HealthSystem>().onDeath.AddListener(DestroyItself);
+        lastTarget = _target.transform.position;
     }
     public override void Update()
     {
         //Go to target
         float step = speed * Time.deltaTime;
         if (_target != null)
-            transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, step);
+            lastTarget = new Vector3(_target.transform.position.x, _target.transform.position.y, _target.transform.position.z);
+        else
+            if (gameObject.transform.position == lastTarget) DestroyItself();
+        transform.position = Vector3.MoveTowards(transform.position, lastTarget, step);
     }
     protected override void OnTriggerEnter2D(Collider2D other)
     {
