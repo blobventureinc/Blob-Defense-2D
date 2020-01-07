c69f34b549a9f08eefafceb5d1a58643ffbe575d
Postea <ydb1997@hotmail.de>
2020-01-07 18:10:22 +0100

    Everything added

---
+++ Assets/Scripts/Player/PlayerActioneer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActioneer : MonoBehaviour
+{
+    [SerializeField] Tile_Targeting targeter = null;
+    [SerializeField] GameObject tower = null;
+    [SerializeField] Player_Mining mining = null;
+    [SerializeField] AttributeManager attributeManager = null;
+    [SerializeField] PlayerTowerAction playerTowerAction = null;
+    [SerializeField] PlayerBuildAction playerBuildAction = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 targetLoc = targeter.gettargetLoc() + new Vector3(0.5f,0.5f,0);
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(targetLoc, 0.3f);
+            Debug.Log(Action(colliders, targetLoc));
+        }
+    }
+    string Action(Collider2D[] colliders, Vector3 targetLoc)
+    {
+        string result = "";
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.name == "Tilemap_noPlacement")
+            {
+                return "NoActions";
+            }
+            else if (colliders[i].gameObject.tag == "Resource")
+            {
+                mining.Mine();
+                result += "Mining";
+            }
+            else if (colliders[i].gameObject.tag == "Tower")
+            {
+                playerTowerAction.Actioning(colliders[i].gameObject);
+                result += "TowerAction";
+            }
+        }
+        if (result == "")
+        {
+            playerBuildAction.Actioning(targetLoc);
+            result += "Build";
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBuildAction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Tile_Targeting), typeof(AttributeManager))]
+public class PlayerBuildAction : MonoBehaviour
+{
+    public GameObject[] towers;
+    public GameObject buildActionMenu;
+    public float reachDistance;
+    bool active = false;
+
+    private Vector3 selectedPosition;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, selectedPosition) > reachDistance)
+        {
+            hideUI();
+        }
+    }
+
+    public void Actioning(Vector3 targetLoc)
+    {
+        if (active) { hideUI(); } else { showUI(); }
+        selectedPosition = targetLoc;
+        //buildActionMenu.transform.position = transform.position;
+    }
+
+    public void hideUI()
+    {
+        buildActionMenu.SetActive(false); active = false;
+    }
+
+    public void showUI()
+    {
+        buildActionMenu.SetActive(true); active = true;
+    }
+
+
+    public void buildTower(int i)
+    {
+        GameObject t = Instantiate(towers[i], selectedPosition, Quaternion.identity) as GameObject;
+        hideUI();
+    }
+}
