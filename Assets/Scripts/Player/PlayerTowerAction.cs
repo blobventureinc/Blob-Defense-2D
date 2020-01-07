7130e234af4285c4b0dcc65377594e5a41cffa0d
Postea <ydb1997@hotmail.de>
2020-01-07 18:08:35 +0100

    Very big changes to tower placement. everything works BUT the search for colliders. Pls enable highlighting of targeted tile.

--- Assets/Scripts/Player/PlayerTowerAction.cs
+++ Assets/Scripts/Player/PlayerTowerAction.cs
@@ -10,7 +10,6 @@ public class PlayerTowerAction : MonoBehaviour
 
     private Tile_Targeting targetingScript = null;
     private AttributeManager attributeScript = null;
-    private Player_Movement movementScript = null;
 
     private Transform selectedTowerTransform;
 
@@ -18,32 +17,24 @@ public class PlayerTowerAction : MonoBehaviour
     {
         targetingScript = GetComponent<Tile_Targeting>();
         attributeScript = GetComponent<AttributeManager>();
-        movementScript = GetComponent<Player_Movement>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.U)) 
+        if (selectedTowerTransform != null)
         {
-            Vector3 target = targetingScript.gettargetLoc();
-            target.x += (float)0.5; target.y += (float)0.5;
-            Vector2 point = new Vector2(target.x, target.y);
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(point, (float)0.5);
-
-            foreach(var collider in colliders) 
+            if (Vector3.Distance(transform.position, selectedTowerTransform.position) > reachDistance)
             {
-                Tower tower = collider.GetComponent<Tower>();
-                if(tower != null) {
-                    towerActionMenu.HideTowerActionMenu();
-                    towerActionMenu.ShowTowerActionMenu(attributeScript, tower);
-                    selectedTowerTransform = collider.transform;
-                    break;
-                }
+                towerActionMenu.HideTowerActionMenu();
             }
         }
-        if(Vector3.Distance(transform.position, selectedTowerTransform.position) > reachDistance) 
-        {
-            towerActionMenu.HideTowerActionMenu();
-        }
+    }
+
+    public void Actioning(GameObject gameObject)
+    {
+        Tower tower = gameObject.GetComponentInChildren<Tower>();
+        towerActionMenu.HideTowerActionMenu();
+        towerActionMenu.ShowTowerActionMenu(attributeScript, tower);
+        selectedTowerTransform = gameObject.transform;
     }
 }
