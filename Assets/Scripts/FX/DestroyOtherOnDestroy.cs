d64196f5d099d04db9c4d86dc127efcbf2bba3e2
MrInformatic <maxundmoritz99199@gmail.com>
2020-01-19 21:56:11 +0100

    Build Sell Particles

---
+++ Assets/Scripts/FX/DestroyOtherOnDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOtherOnDestroy : MonoBehaviour
+{
+    public GameObject obj;
+    
+    void OnDestroy() 
+    {
+        Destroy(obj);
+    }
+}
---
+++ Assets/Scripts/FX/SpawnObjectOnDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectOnDestroy : MonoBehaviour
+{
+    public GameObject obj;
+
+    void OnDestroy() 
+    {
+        Instantiate(obj, transform.position, Quaternion.identity);
+    }
+}
