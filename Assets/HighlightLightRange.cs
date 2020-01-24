47beefd917b2f49583aaac16952cc6d7aab446f4
Florian Taute <info@tauteweb.de>
2020-01-25 00:18:15 +0100

    sprite stuff

--- Assets/HighlightLightRange.cs
+++ Assets/HighlightLightRange.cs
@@ -12,15 +12,4 @@ public class HighlightLightRange : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = false;
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
