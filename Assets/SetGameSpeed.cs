5ad8d2025ab0aed82f69d1ce6f4a3ec71d33b2df
Florian Taute <info@tauteweb.de>
2020-01-27 00:47:51 +0100

    Enemy Prefabs / Balancing / Playable Until Round 7

---
+++ Assets/SetGameSpeed.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGameSpeed : MonoBehaviour
+{
+    public void Set(float value) {
+        Time.timeScale = value;
+    }
+}
